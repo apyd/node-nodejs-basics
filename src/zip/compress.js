fdbea8d86ec5e6f4270072a7a122891b0c4ef2e4
apyd <arturpydych@gmail.com>
2024-01-28 23:31:58 +0100

    feat: implement zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip()
+  
+  const currentDir = dirname(fileURLToPath(import.meta.url))
+  const source = createReadStream(join(currentDir, 'files/fileToCompress.txt'))
+  const destination = createWriteStream(join(currentDir, 'files/archive.gz'))
+
+  const pipelineAsync = promisify(pipeline);
+
+  try {
+    await pipelineAsync(source, gzip, destination)
+  }
+  catch (error) {
+    console.error('Error occurred: ', error)
+    process.exitCode = 1
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { join, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util';
+
 const decompress = async () => {
-    // Write your code here 
+  const unzip = createGunzip()
+  
+  const currentDir = dirname(fileURLToPath(import.meta.url))
+  const source = createReadStream(join(currentDir, 'files/archive.gz'))
+  const destination = createWriteStream(join(currentDir, 'files/fileToCompress.txt'))
+
+  const pipelineAsync = promisify(pipeline);
+
+  try {
+    await pipelineAsync(source, unzip, destination)
+  }
+  catch (error) {
+    console.error('Error occurred: ', error)
+    process.exitCode = 1
+  }
 };
 
 await decompress();
