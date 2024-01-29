1cfa29289f7485203ba8abace36d6c0ccd7ebc0c
apyd <arturpydych@gmail.com>
2024-01-29 15:05:47 +0100

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = join(dirname(fileURLToPath(import.meta.url)), 'files/script.js')
+    const child = spawn('node', [scriptPath, ...args]);
+    
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
