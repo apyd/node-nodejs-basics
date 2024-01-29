351ad8887477f5985ac211d4b74c7f2dd1c52a79
apyd <arturpydych@gmail.com>
2024-01-29 15:32:03 +0100

    refactor: shorten stdin/stdout by import

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,13 +1,14 @@
 import { join, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { stdin, stdout } from 'node:process';
 import { spawn } from 'node:child_process';
 
 const spawnChildProcess = async (args) => {
     const scriptPath = join(dirname(fileURLToPath(import.meta.url)), 'files/script.js')
     const child = spawn('node', [scriptPath, ...args]);
     
-    process.stdin.pipe(child.stdin);
-    child.stdout.pipe(process.stdout);
+    stdin.pipe(child.stdin);
+    child.stdout.pipe(stdout);
 };
 
 // Put your arguments in function call to test this functionality
